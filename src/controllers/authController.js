9096e1886bb53a4fe37d60f76a3aec685326aa09
Paulo-P-33 <luiz.lira@a.ficr.edu.br>
2021-12-03 23:09:44 -0300

    criação de novas rotas e logins

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,7 @@ const express = require('express');
 const User = require('../models/cadastroUser.js');
 const UserClinica = require('../models/cadastroClinica.js')
 
+
 const router = express.Router();
 
 router.post('/registerUser', async (req, res) => {
@@ -28,6 +29,36 @@ router.post('/registerClinica', async (req, res) => {
     return res.status(400).send({ error: 'Registration failed' });
   }
 });
+
+router.post('/loginUser', async (req, res) => {
+const { email, senha } = req.body;
+
+const user = await User.findOne({ email }).select('+senha');
+
+if (!user)
+  return res.status(400).send({ error: 'User not found' });
+
+if (senha != user.senha)
+  return res.status(400).send({error: 'Senha inválida'});
+
+res.send({user});
+
+});
+
+router.post('/loginClinica', async (req, res) => {
+  const { cnpj, senha } = req.body;
+  
+  const clinica = await UserClinica.findOne({ cnpj }).select('+senha');
+  
+  if (!clinica)
+    return res.status(400).send({ error: 'User not found' });
+  
+  if (senha != clinica.senha)
+    return res.status(400).send({error: 'Senha inválida'});
+  
+  res.send({clinica});
+  
+  });
 
 
 module.exports = app => app.use('/auth', router);
